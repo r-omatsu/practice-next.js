2ff3679d37e6ea3aaebd0fdb16d7bbcddfeffbdf
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-07-13 21:01:54 +0900

    useRouterを使用したカテゴリー一覧へ戻るを実装

--- my-app/src/app/categories/[categoryName]/page.tsx
+++ my-app/src/app/categories/[categoryName]/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 type Props = {
   params: { categoryName: string };
   searchParams: { [key: string]: string | string[] | undefined };
@@ -5,11 +9,19 @@ type Props = {
 
 export default function Page({ params, searchParams }: Props) {
   const page = typeof searchParams.page === "string" ? searchParams.page : "1";
+  const router = useRouter();
   return (
     <div>
       <h1>カテゴリー別一覧画面</h1>
       <h2>カテゴリー「{params.categoryName}」</h2>
       <p>ページ番号: 「{page}」</p>
+      <button
+        onClick={() => {
+          router.push("/categories");
+        }}
+      >
+        カテゴリー一覧へ戻る
+      </button>
     </div>
   );
 }
