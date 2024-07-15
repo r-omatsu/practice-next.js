ed369cfed4d0cfb09802f2d68025ecbe89afa184
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-07-15 13:46:28 +0900

    静的レンダリングと動的レンダリングについて学習 ざっくり言うと、サーバーサイドで解決できるような実装であれば静的レンダリングとなり、 クライアントの情報が必要な実装（動的データ取得の使用、動的関数の使用、Dynamic Segmentの使用）があれば動的レンダリングになる

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,6 +5,9 @@ import { Photo } from "./utils/type";
 async function getPhotos() {
   const data: { photos: Photo[] } = await fetch(
     "http://localhost:8080/api/photos",
+    // NOTE: キャッシュを利用しない設定のためこのページは動的レンダリングになる
+    // NOTE: ここをコメントアウトすると静的レンダリングになる
+    // NOTE: 静的レンダリングにした場合、APIサーバーが立ち上がっていないとnpm run clean-buildに失敗する（npm run buildだとキャッシュを利用するので、成功することもあるので注意）
     { cache: "no-store" }
   ).then((res) => res.json());
 
