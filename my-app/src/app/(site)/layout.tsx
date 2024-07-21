bae69b961917512d52afd14d37ae344bb475faf0
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-07-21 22:07:34 +0900

    4-1 Rotue Handlerの定義方法について学習 対応するルートにroute.tsを定義し、HTTPメソッドに応じた命名でメソッドを定義することで作成できる 引数はNextRequest、戻り値はNextResponse..だが、Requestなどもあるようなので、違いは要確認 ルートのコンフリクトはnext devでは気付けないので注意！

--- my-app/src/app/(site)/layout.tsx
+++ my-app/src/app/(site)/layout.tsx
@@ -16,6 +16,9 @@ export default function Layout({ children, users }: Props) {
       <p>Site layout</p>
       {children}
       {users}
+      <form action="api/hello" method="post">
+        <button type="submit">Request POST Hello API</button>
+      </form>
     </div>
   );
 }
--- my-app/src/app/api/hello/route.ts
+++ my-app/src/app/api/hello/route.ts
@@ -1,3 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
 export async function GET(request: Request) {
   return new Response("Hello, Next.js!");
+}
+
+export async function POST(request: NextRequest) {
+  return NextResponse.json({ liked: true }, { status: 404 });
 }
