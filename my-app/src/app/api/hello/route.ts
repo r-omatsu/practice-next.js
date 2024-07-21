49605287b4706b18f123af8dd636578fe5e8386e
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-07-21 22:22:02 +0900

    4-2 Route Handlerのレンダリングについて学習 Route Handlerには静的Route Handlerと動的Route Handlerがある - 静的Route Handler   - レスポンスボディをあらかじめキャッシュファイルとして出力している   - リクエストのたびに外部Web APIサーバーからデータを取得したりしない   - 静的データ取得関数を使用している場合、Next.jsは同じキャッシュファイルを使用してレスポンスを返す - 動的Route Handler   - 次の5つの要因のいずれかを満たす場合、動的Route Handlerになる     1. Dynamic Segment値の参照        - exportする関数の第2引数がDynamic Segment値として参照できる     2. Request（NextRequest）オブジェクトの参照        - 第1引数がRequestオブジェクトであり、request.urlなどの記述で参照できる     3. 動的関数の使用        - 動的関数にはcookiesやheadersなどがある     4. GETとHEAD以外のHTTP関数のexport        - POSTとかが含まれる場合は動的Route Handlerになる     5. Segment Config Optionsの指定        - 次のように指定でき、指定すると強制的に動的Route Handlerになる        - export const dynamic = "force-dynamic";

--- my-app/src/app/api/hello/route.ts
+++ my-app/src/app/api/hello/route.ts
@@ -4,6 +4,7 @@ export async function GET(request: Request) {
   return new Response("Hello, Next.js!");
 }
 
+// NOTE: GET,HEAD以外のHTTP関数のexportは動的Route Handlerの原因となる
 export async function POST(request: NextRequest) {
   return NextResponse.json({ liked: true }, { status: 404 });
 }
